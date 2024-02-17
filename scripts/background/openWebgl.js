88183d82a930597545cfdce0c98992fcd9c52c5c
jorelwall451 <jorelwall154@gmail.com>
2024-02-16 21:25:24 -0300

    Add openWebgl

---
+++ scripts/background/openWebgl.js
@@ -0,0 +1,24 @@
+
+/**
+ * 
+ * @author {eminent}
+ */
+
+export default function openWebgl(lang){
+    const langValue = lang;
+
+    const xhrPrototype = XMLHttpRequest.prototype;
+
+    const originalOpenFunction = xhrPrototype.open;
+
+    xhrPrototype.open = function () {
+        const url = arguments[1];
+        if (url.includes("lang=")) {
+            const newUrl = url.replace(/(lang=)[^&]*/, `$1${langValue}`);
+            arguments[1] = newUrl;
+            console.log("URL alterada para incluir 'lang=" + langValue + "'");
+        }
+
+        originalOpenFunction.apply(this, arguments);
+    };
+}
