7d2b22102ccb8c6bc98d37c34befa8b2ac212945
jorelwall451 <jorelwall154@gmail.com>
2024-02-18 16:50:33 -0300

    update somethings

--- scripts/background/getDefaultFieldsValues.js
+++ scripts/background/getDefaultFieldsValues.js
@@ -1,29 +1,25 @@
-function getProfileID(){
-  /**
-   * @type {HTMLMetaElement}
-   */
-  const metaProfileID = document.querySelector("meta[name='Notify-Token']");
-  const profileID = metaProfileID.content.split(".")[0];
 
+function getProfileID() {
+  const metaProfileID = document.querySelector("meta[name='Notify-Token']");
+  if (!metaProfileID) {
+    return null;
+  }
+  const profileID = metaProfileID.content.split(".")[0];
   return profileID;
 }
 
-function getPlanetID(){
+function getPlanetID() {
   const url = window.location.href;
-  
   const regex = /\/play\/(\d+)/;
-  
   const match = url.match(regex);
-  
   const planetID = match ? match[1] : null;
-
   return planetID;
 }
 
 export default function getDefaultFieldsValues() {
   const profileID = getProfileID();
   const planetID = getPlanetID();
-  
+
   return {
     profileID,
     planetID,
